8284a81989088dd3f7e713e8bf959080958084a1
SerhiiKhyzhko <serhiyh1999@gmail.com>
2021-02-01 20:04:08 +0200

    Login Page
    Added continue button to redirect to registration page

--- oct/pages/models/login_page.py
+++ oct/pages/models/login_page.py
@@ -1,5 +1,11 @@
 from pages.base.page import BasePage
-from pages.base.elements import Form, Block, InputField
+from pages.base.elements import Form, Block, InputField, Clickable
+
+
+class NewCustomButton(Block):
+    contains = {
+        "button": {"locator":("XPATH", "//*[@id=\"content\"]/div/div[1]/div/a"), "class": Clickable}
+    }
 
 
 class ReturningCustomer(Block):
@@ -19,9 +25,19 @@ class LoginForm(Form):
         self.personal.password.fill(kwargs.get("password"))
 
 
+class NewCustomer(Block):
+    contains = {
+        'action': {"locator": ("CLASS_NAME", "btn-primary"), "class": NewCustomerButton}
+    }
+
+    def click_button(self):
+        self.action.button.click()
+
+
 class LoginPage(BasePage):
     url = "index.php?route=account/login"
 
     contains = {
         "form": {"locator": ("XPATH", "//*[@id=\"content\"]/div/div[2]/div/form"), "class": LoginForm},
+        "action": {"locator": ("XPATH", "//*[@id=\"content\"]/div/div[1]/div"), "class": NewCustomer}
     }
