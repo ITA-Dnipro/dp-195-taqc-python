cd9f412de087881ee285d012886bd3dfeb8de0ee
yura <yuralrock@gmail.com>
2021-02-16 21:59:34 +0200

    fix

--- oct/tests/shopping_cart_test.py
+++ oct/tests/shopping_cart_test.py
@@ -9,16 +9,16 @@ from oct.tests import test_run
 
 class ShoppingCartTest(Testcase):
     @setup
-    def open(self, host, driver, email, password) -> None:
+    def open(self, protocol, host, driver, email, password) -> None:
         self.page = ProductPage(driver)
-        self.page.load(host)
-        self.page.add_logged_in_cookie_session(host, email, password)
+        self.page.load(protocol, host)
+        self.page.add_logged_in_cookie_session(protocol, host, email, password)
         self.page.cart.add()
 
     @test
-    def test_submit(self, host, driver, gift_certificate: str = "100dollars") -> None:
+    def test_submit(self, protocol, host, driver, gift_certificate: str = "100dollars") -> None:
         self.page = ShoppingCartPage(driver)
-        self.page.load(host)
+        self.page.load(protocol, host)
         self.page.apply_gift_certificate(gift_certificate)
         actual_result = self.page.sub_total + self.page.discount(gift_certificate)
         expected_result = self.page.total
