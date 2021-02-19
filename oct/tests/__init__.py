51c8a01208c2f5d5f8e2f31850b71dac182af1db
yura <yuralrock@gmail.com>
2021-02-19 17:50:27 +0200

    Added job file

--- oct/tests/__init__.py
+++ oct/tests/__init__.py
@@ -1,5 +1,3 @@
-import os
-
 from pyats import aetest
 from pyats.topology import loader, Testbed
 
@@ -8,21 +6,17 @@ from settings import email, customer_password, browser, protocol
 
 
 TESTBED = "testbed.yaml"
-DATAFILE = os.path.join(os.path.dirname(__file__), "datafile", "common_data.yaml")
 
 
-def test_run(testbed_file: str = TESTBED, datafile: str = DATAFILE) -> None:
+def test_run(testbed_file: str = TESTBED) -> None:
 
     testbed = loader.load(testbed_file)
 
-    aetest.main(
-        **mandatory_test_arguments(testbed, datafile), email=email, password=customer_password
-    )
+    aetest.main(**mandatory_test_arguments(testbed), email=email, password=customer_password)
 
 
 def mandatory_test_arguments(
     testbed: Testbed,
-    datafile: str,
     test_browser: str = browser,
     app_protocol: str = protocol,
 ) -> dict:
@@ -38,5 +32,4 @@ def mandatory_test_arguments(
         "grid": selenium_grid,
         "protocol": app_protocol,
         "host": host,
-        "datafile": datafile,
     }
