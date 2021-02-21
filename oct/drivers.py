55650f7cc9035ec954e9062ef1777cbfee6a20fc
YuriiShp <69315230+YuriiShp@users.noreply.github.com>
2021-02-21 13:03:52 +0200

    Add remote driver delay handling (#33)
    * Fixed issue related to the remote driver initialization delay
    
    1. Edited module oct/drivers.py (added polling)
    2. Added polling2 to the requirements
    
    * Reformated oct/drivers.py module

--- oct/drivers.py
+++ oct/drivers.py
@@ -1,9 +1,40 @@
 import importlib
 
+import polling2
+import requests
+from requests.exceptions import ConnectionError as RequestConnectionError
 from selenium import webdriver
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.common.exceptions import WebDriverException
 from webdriver_manager.utils import ChromeType
+
+
+def reliable_remote_initialization(driver_parameters) -> WebDriver:
+
+    # poll server connection
+    try:
+        url = driver_parameters["command_executor"].replace("/wd/hub", "")
+        polling2.poll(
+            lambda: requests.get(url).status_code == 200,  # noqa
+            ignore_exceptions=(RequestConnectionError,),
+            step=1,
+            timeout=10,
+        )
+    except polling2.TimeoutException as error:
+        raise ConnectionError("Could not connect to Selenium server") from error
+
+    # poll driver initialization
+    try:
+        output = polling2.poll(
+            lambda: webdriver.Remote(**driver_parameters),  # noqa
+            ignore_exceptions=(WebDriverException,),
+            step=1,
+            timeout=10,
+        )
+        return output
+    except polling2.TimeoutException as error:
+        raise WebDriverException("Could not start new Driver session") from error
 
 
 def get_driver(browser: str, grid: str) -> WebDriver:
@@ -17,9 +48,14 @@ def get_driver(browser: str, grid: str) -> WebDriver:
         options.add_argument("--ignore-certificate-errors")
         options.add_argument("--remote-debugging-port=9222")
         options.headless = True
-        output = webdriver.Remote(
-            command_executor=grid, desired_capabilities=DesiredCapabilities.CHROME, options=options
-        )
+
+        remote_mandatory_params = {
+            "command_executor": grid,
+            "desired_capabilities": DesiredCapabilities.CHROME,
+            "options": options,
+        }
+
+        output = reliable_remote_initialization(driver_parameters=remote_mandatory_params)
 
     # run tests locally
     elif browser == "chrome":
