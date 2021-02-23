f7612ba48346e0a472f1a5a73b25117afd1e0582
Nick <36864656+N1ki4@users.noreply.github.com>
2021-02-23 18:00:33 +0200

    Edit Address Book Page (#27)
    * FIX Address Book Page
    
    Add class AddressBookPage with items inside contains
    
    * FIX Address Book Page
    
    Second commit, changed black linting
    
    Co-authored-by: YuriiShp <69315230+YuriiShp@users.noreply.github.com>

--- oct/pages/models/address_book.py
+++ oct/pages/models/address_book.py
@@ -1,4 +1,4 @@
-# from oct.pages.base.page import BasePage
+from oct.pages.base.page import BasePage
 from oct.pages.base.elements import Block, Clickable
 
 
@@ -15,5 +15,26 @@ class ButtonsClearfix(Clickable):
         "new_address_button": {
             "locator": ("CSS_SELECTOR", "a.btn.btn-primary"),
             "class": Clickable,
+        },
+    }
+
+
+class AddressBookPage(BasePage):
+    url = "index.php?route=account/address"
+
+    contains = {
+        "edit_delete_buttons": {
+            "locator": (
+                "XPATH",
+                "//*[@id='content']/div[1]/table/tbody/tr/td[2]",
+            ),
+            "class": AddressBookEntries,
+        },
+        "new_address_and_back": {
+            "locator": (
+                "XPATH",
+                "//*[@id='content']/div[2]",
+            ),
+            "class": ButtonsClearfix,
         },
     }
